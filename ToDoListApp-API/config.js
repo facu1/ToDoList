87edcf74a31bf6914b7abcf364c86d73636d2dd3
facu1 <fac.gonzalez@outlook.com>
2020-11-22 21:36:13 -0300

    ultimo cambio

--- ToDoListApp-API/config.js
+++ ToDoListApp-API/config.js
@@ -1,5 +1,10 @@
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
+  // Aclaracion importante!
+  // Lo que esta a continuacin no se deberia hacer, pq se esta mostrando la base 
+  // de datos donde se guarda todo lo de la app, se deberia definir en las 
+  // variables de entorno de heroku, yo lo defino asi para que no  se tenga que 
+  // instalar MongoDB en su computadora
   dbUrl: process.env.DB_URL || 'mongodb+srv://userQuasar:quasar@cluster0.qhync.mongodb.net/prueba1?retryWrites=true&w=majority',
   port: process.env.PORT || 5000,
   host: process.env.HOST || 'http://localhost',
